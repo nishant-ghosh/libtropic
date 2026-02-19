5b7e3e787ae83a5f1312cfff5b2a003eb3f16397
andreondra <golasowski.o@gmail.com>
2026-02-19 15:08:11 +0100

    doc(lt_ecc_ecdsa_sign): made comments clearer

--- include/libtropic.h
+++ include/libtropic.h
@@ -549,18 +549,19 @@ lt_ret_t lt_ecc_key_read(lt_handle_t *h, const lt_ecc_slot_t ecc_slot, uint8_t *
 lt_ret_t lt_ecc_key_erase(lt_handle_t *h, const lt_ecc_slot_t ecc_slot);
 
 /**
- * @brief Performs ECDSA sign of a message with a private ECC key stored in TROPIC01
+ * @brief Calculates ECDSA signature of a message hash (32 bytes) with a private ECC key stored in
+ * TROPIC01.
  *
- * @param h            Handle for communication with TROPIC01
- * @param ecc_slot     Slot containing a private key, TR01_ECC_SLOT_0 - TR01_ECC_SLOT_31
- * @param msg_hash     Buffer containing a message
- * @param msg_hash_len Length of the message
+ * @param h            Handle for communication with TROPIC01.
+ * @param ecc_slot     Slot containing a private key, TR01_ECC_SLOT_0 - TR01_ECC_SLOT_31.
+ * @param msg_hash     Buffer containing a 32-byte hash of the message to sign.
+ * @param msg_hash_len Length of the hash.
  * @param rs           Buffer for storing a signature in a form of R and S bytes (should always have
- * length 64B)
+ * length 64B).
  *
- * @retval             LT_OK Function executed successfully
+ * @retval             LT_OK Function executed successfully.
  * @retval             other Function did not execute successully, you might use lt_ret_verbose() to
- * get verbose encoding of returned value
+ * get verbose encoding of returned value.
  */
 lt_ret_t lt_ecc_ecdsa_sign(lt_handle_t *h, const lt_ecc_slot_t ecc_slot, const uint8_t *msg_hash,
                            const uint32_t msg_hash_len, uint8_t *rs);
