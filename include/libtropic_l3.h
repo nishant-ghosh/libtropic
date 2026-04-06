7a4aa5ef1f5e6b37e645b2133fb368500b0b150f
andreondra <golasowski.o@gmail.com>
2026-04-06 14:26:49 +0200

    doc(libtropic.h,libtropic_l3.h): removed periods from sentences in comments.

--- include/libtropic_l3.h
+++ include/libtropic_l3.h
@@ -463,11 +463,11 @@ lt_ret_t lt_in__ecc_key_erase(lt_handle_t *h);
  * @note Used for separate L3 communication, for more information read info
  * at the top of this file.
  *
- * @param h            Handle for communication with TROPIC01.
- * @param slot         ECC key slot to use for signing.
- * @param msg_hash     Buffer containing a 32-byte hash of the message to sign.
- * @param msg_hash_len Length of the hash.
- * @return             LT_OK if success, otherwise returns other error code.
+ * @param h            Handle for communication with TROPIC01
+ * @param slot         ECC key slot to use for signing
+ * @param msg_hash     Buffer containing a 32-byte hash of the message to sign
+ * @param msg_hash_len Length of the hash
+ * @return             LT_OK if success, otherwise returns other error code
  */
 lt_ret_t lt_out__ecc_ecdsa_sign(lt_handle_t *h, const lt_ecc_slot_t slot, const uint8_t *msg_hash,
                                 const uint32_t msg_hash_len);
