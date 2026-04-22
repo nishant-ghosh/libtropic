853eb27a1b090144fe96fc99ff3f025bb2b5e99a
Ondrej Golasowski <golasowski.o@gmail.com>
2026-04-22 13:46:25 +0200

    examples(nucleo_u545r_q/hello_world): added contents of the example

--- examples/stm32/nucleo_u545re_q/hello_world/Src/main.c
+++ examples/stm32/nucleo_u545re_q/hello_world/Src/main.c
@@ -1,6 +1,6 @@
 /**
  * @file main.c
- * @brief Wrapper project for running Libtropic functional test suite on Nucleo U545RE-Q.
+ * @brief Simple "Hello, World!" example of using Libtropic on Nucleo U545RE-Q board.
  * @copyright Copyright (c) 2020-2026 Tropic Square s.r.o.
  *
  * @license For the license see LICENSE.md in the root directory of this source tree.
@@ -22,26 +22,27 @@
 #include "main.h"
 
 #include <inttypes.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "libtropic.h"
-#include "libtropic_functional_tests.h"
-#include "libtropic_logging.h"
-#include "libtropic_port_stm32u5xx.h"
-#include "lt_test_common.h"
-
-#if LT_USE_TREZOR_CRYPTO
-#include "libtropic_trezor_crypto.h"
-#define CRYPTO_CTX_TYPE lt_ctx_trezor_crypto_t
-#elif LT_USE_MBEDTLS_V4
 #include "libtropic_mbedtls_v4.h"
+#include "libtropic_port_stm32u5xx.h"
 #include "psa/crypto.h"
-#define CRYPTO_CTX_TYPE lt_ctx_mbedtls_v4_t
-#elif LT_USE_WOLFCRYPT
-#include "libtropic_wolfcrypt.h"
-#include "wolfssl/wolfcrypt/error-crypt.h"
-#include "wolfssl/wolfcrypt/wc_port.h"
-#define CRYPTO_CTX_TYPE lt_ctx_wolfcrypt_t
+
+/* Private define ------------------------------------------------------------*/
+/* Message to send with Ping L3 command. */
+#define PING_MSG "This is Hello World message from TROPIC01!!"
+/* Size of the Ping message, including '\0'. */
+#define PING_MSG_SIZE 44
+
+/* Choose pairing keypair for slot 0. */
+#if LT_USE_SH0_ENG_SAMPLE
+#define LT_EX_SH0_PRIV lt_sh0priv_eng_sample
+#define LT_EX_SH0_PUB lt_sh0pub_eng_sample
+#elif LT_USE_SH0_PROD0
+#define LT_EX_SH0_PRIV lt_sh0priv_prod0
+#define LT_EX_SH0_PUB lt_sh0pub_prod0
 #endif
 
 /* Private variables ---------------------------------------------------------*/
@@ -104,25 +105,35 @@ int main(void)
     /* libtropic related code BEGIN */
     /* libtropic related code BEGIN */
 
-    /* Cryptographic function provider initialization. */
-#if LT_USE_MBEDTLS_V4
+    printf("======================================\n");
+    printf("==== TROPIC01 Hello World Example ====\n");
+    printf("======================================\n");
+
+    /* Cryptographic function provider initialization.
+
+        In production, this would typically be done only once,
+        usually at the start of the application or before
+        the first use of cryptographic functions but no later than
+        the first occurrence of any Libtropic function */
     psa_status_t status = psa_crypto_init();
     if (status != PSA_SUCCESS) {
-        LT_LOG_ERROR("PSA Crypto initialization failed, status=%d (psa_status_t)", status);
+        fprintf(stderr, "PSA Crypto initialization failed, status=%ld (psa_status_t)\n", status);
         return -1;
     }
-#elif LT_USE_WOLFCRYPT
-    int ret = wolfCrypt_Init();
-    if (ret != 0) {
-        LT_LOG_ERROR("WolfCrypt initialization failed, ret=%d (%s)", ret, wc_GetErrorString(ret));
-        return ret;
-    }
-#endif
 
-    /* Libtropic handle initialization */
+    /* Libtropic handle.
+
+        It is declared here (on stack) for
+        simplicity. In production, you put it on heap if needed. */
     lt_handle_t lt_handle = {0};
 
-    /* Device mappings */
+    /* Device structure.
+
+        Modify this according to your environment. Default values
+        are compatible with RPi and our RPi shield.
+
+        The device structure has to be zero initialized!
+        STM32 HAL depends on zero init values. */
     lt_dev_stm32u5xx_t device = {0};
 
     device.spi_instance = SPI1;
@@ -138,28 +149,87 @@ int main(void)
 
     lt_handle.l2.device = &device;
 
-    /* Crypto abstraction layer (CAL) context (selectable). */
-    CRYPTO_CTX_TYPE crypto_ctx;
+    /* Crypto abstraction layer (CAL) context. */
+    lt_ctx_mbedtls_v4_t crypto_ctx;
     lt_handle.l3.crypto_ctx = &crypto_ctx;
 
-    /* Test code (correct test function is selected automatically per binary)
-       __lt_handle__ identifier is used by the test registry. */
-    lt_handle_t *__lt_handle__ = &lt_handle;
-#include "lt_test_registry.c.inc"
 
-    /* Cryptographic function provider deinitialization. */
-#if LT_USE_MBEDTLS_V4
-    mbedtls_psa_crypto_free();
-#elif LT_USE_WOLFCRYPT
-    ret = wolfCrypt_Cleanup();
-    if (ret != 0) {
-        LT_LOG_ERROR("WolfCrypt cleanup failed, ret=%d (%s)", ret, wc_GetErrorString(ret));
-        return ret;
+    printf("Initializing handle...");
+    lt_ret_t ret = lt_init(&lt_handle);
+    if (LT_OK != ret) {
+        fprintf(stderr, "\nFailed to initialize handle, ret=%s\n", lt_ret_verbose(ret));
+        mbedtls_psa_crypto_free();
+        return -1;
     }
-#endif
+    printf("OK\n");
 
-    /* Inform the test runner that the test finished */
-    LT_FINISH_TEST();
+    /* We need to ensure we are not in the Startup Mode, as L3 commands are available only in the
+     * Application Firmware.
+     */
+    printf("Sending reboot request...");
+    ret = lt_reboot(&lt_handle, TR01_REBOOT);
+    if (ret != LT_OK) {
+        fprintf(stderr, "\nlt_reboot() failed, ret=%s\n", lt_ret_verbose(ret));
+        lt_deinit(&lt_handle);
+        mbedtls_psa_crypto_free();
+        return -1;
+    }
+    printf("OK\n");
+
+    printf("Starting Secure Session with key slot %d...", (int)TR01_PAIRING_KEY_SLOT_INDEX_0);
+    /* Keys are chosen based on the CMake option LT_SH0_KEYS. */
+    ret = lt_verify_chip_and_start_secure_session(&lt_handle, LT_EX_SH0_PRIV, LT_EX_SH0_PUB,
+                                                  TR01_PAIRING_KEY_SLOT_INDEX_0);
+    if (LT_OK != ret) {
+        fprintf(stderr, "\nFailed to start Secure Session with key %d, ret=%s\n",
+                (int)TR01_PAIRING_KEY_SLOT_INDEX_0, lt_ret_verbose(ret));
+        fprintf(stderr,
+                "Check if you use correct SH0 keys! Hint: if you use an engineering sample chip, "
+                "compile with "
+                "-DLT_SH0_KEYS=eng_sample\n");
+        lt_deinit(&lt_handle);
+        mbedtls_psa_crypto_free();
+        return -1;
+    }
+    printf("OK\n");
+
+    uint8_t recv_buf[PING_MSG_SIZE];
+    printf("Sending Ping command...\n");
+    printf("\t--> Message sent to TROPIC01: '%s'\n", PING_MSG);
+    ret = lt_ping(&lt_handle, (const uint8_t *)PING_MSG, recv_buf, PING_MSG_SIZE);
+    if (LT_OK != ret) {
+        fprintf(stderr, "Ping command failed, ret=%s\n", lt_ret_verbose(ret));
+        lt_session_abort(&lt_handle);
+        lt_deinit(&lt_handle);
+        mbedtls_psa_crypto_free();
+        return -1;
+    }
+    printf("\t<-- Message received from TROPIC01: '%s'\n", recv_buf);
+
+    printf("Aborting Secure Session...");
+    ret = lt_session_abort(&lt_handle);
+    if (LT_OK != ret) {
+        fprintf(stderr, "\nFailed to abort Secure Session, ret=%s\n", lt_ret_verbose(ret));
+        lt_deinit(&lt_handle);
+        mbedtls_psa_crypto_free();
+        return -1;
+    }
+    printf("OK\n");
+
+    printf("Deinitializing handle...");
+    ret = lt_deinit(&lt_handle);
+    if (LT_OK != ret) {
+        fprintf(stderr, "\nFailed to deinitialize handle, ret=%s\n", lt_ret_verbose(ret));
+        mbedtls_psa_crypto_free();
+        return -1;
+    }
+    printf("OK\n");
+
+    /* Cryptographic function provider deinitialization.
+
+        In production, this would be done only once, typically
+        during termination of the application. */
+    mbedtls_psa_crypto_free();
 
     /* libtropic related code END */
     /* libtropic related code END */
