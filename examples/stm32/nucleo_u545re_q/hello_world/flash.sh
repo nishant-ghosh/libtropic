6222e99b2447f78e431222f60d8056e012b1e318
andreondra <golasowski.o@gmail.com>
2026-04-22 12:56:22 +0200

    examples(nucleo_u545re_q/hello_world): initial commit

---
+++ examples/stm32/nucleo_u545re_q/hello_world/flash.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BINARY_PATH="$1"
+STLINK_SERIAL_NUMBER="$2"
+# Get the directory where this script is located.
+SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" &> /dev/null && pwd)
+
+if [ -z $1 ]; then
+    echo "Libtropic NUCLEO U545RE-Q flash script"
+    echo "usage: ./flash.sh PATH_TO_BINARY [STLINK_SERIAL_NUMBER]"
+    exit 1
+fi
+
+if [ -z "$STLINK_SERIAL_NUMBER" ]; then
+    echo "No STLink serial number provided, OpenOCD will autodiscover STLink programming interface."
+    openocd -f "$SCRIPT_DIR/nucleo-u5xx.cfg" -c "program $1 verify reset exit"
+else
+    echo "Using STLink serial number: $STLINK_SERIAL_NUMBER"
+    openocd -f "$SCRIPT_DIR/nucleo-u5xx.cfg" -c "adapter serial $STLINK_SERIAL_NUMBER" -c "program $1 verify reset exit"
+fi
