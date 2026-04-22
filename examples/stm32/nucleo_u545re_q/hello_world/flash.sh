b52821ae8881ca03b619c85ee4fcda167396a6cc
Ondrej Golasowski <golasowski.o@gmail.com>
2026-04-22 14:46:11 +0200

    examples(nucleo_u545r_q/hello_world): enquote parameter in flash.sh script

--- examples/stm32/nucleo_u545re_q/hello_world/flash.sh
+++ examples/stm32/nucleo_u545re_q/hello_world/flash.sh
@@ -5,7 +5,7 @@ STLINK_SERIAL_NUMBER="$2"
 # Get the directory where this script is located.
 SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" &> /dev/null && pwd)
 
-if [ -z $1 ]; then
+if [ -z "$BINARY_PATH" ]; then
     echo "Libtropic NUCLEO U545RE-Q flash script"
     echo "usage: ./flash.sh PATH_TO_BINARY [STLINK_SERIAL_NUMBER]"
     exit 1
@@ -13,8 +13,8 @@ fi
 
 if [ -z "$STLINK_SERIAL_NUMBER" ]; then
     echo "No STLink serial number provided, OpenOCD will autodiscover STLink programming interface."
-    openocd -f "$SCRIPT_DIR/nucleo-u5xx.cfg" -c "program $1 verify reset exit"
+    openocd -f "$SCRIPT_DIR/nucleo-u5xx.cfg" -c "program "$BINARY_PATH" verify reset exit"
 else
     echo "Using STLink serial number: $STLINK_SERIAL_NUMBER"
-    openocd -f "$SCRIPT_DIR/nucleo-u5xx.cfg" -c "adapter serial $STLINK_SERIAL_NUMBER" -c "program $1 verify reset exit"
+    openocd -f "$SCRIPT_DIR/nucleo-u5xx.cfg" -c "adapter serial $STLINK_SERIAL_NUMBER" -c "program "$BINARY_PATH" verify reset exit"
 fi
