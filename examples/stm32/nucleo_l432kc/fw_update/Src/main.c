12aa9f8b5f74798af2d045bbd7533cb93772734d
Jan Medek <czmedexs@gmail.com>
2026-04-27 15:58:24 +0200

    feat(examples/stm32/): add maintenance mode control to fw update example

--- examples/stm32/nucleo_l432kc/fw_update/Src/main.c
+++ examples/stm32/nucleo_l432kc/fw_update/Src/main.c
@@ -68,6 +68,9 @@
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 
+/* Tracks whether the Maintenance Mode had to be enabled before starting the FW update. */
+bool g_mtnc_mode_was_enabled = false;
+
 /* Private function prototypes -----------------------------------------------*/
 #ifdef __GNUC__
 /* With GCC, small printf (option LD Linker->Libraries->Small printf
@@ -169,6 +172,189 @@ PUTCHAR_PROTOTYPE
     }
 
     return ch;
+}
+
+/**
+ * @brief  Checks if Maintenance Mode is enabled and if not, enables it in R-Config.
+ * @param  lt_handle_t Device handle
+ * @retval LT_OK on success
+ */
+lt_ret_t check_and_enable_maintenance_mode(lt_handle_t *lt_handle)
+{
+    lt_ret_t ret;
+
+    printf("\nChecking if Maintenance Mode is enabled and enabling it in R-Config if needed:\n");
+
+    // Establish Secure Channel Session so we can read I-Config/R-Config.
+    printf("  - Starting Secure Session with key slot %d...", (int)TR01_PAIRING_KEY_SLOT_INDEX_0);
+    // Keys are chosen based on the CMake option LT_SH0_KEYS.
+    ret = lt_verify_chip_and_start_secure_session(lt_handle, LT_EX_SH0_PRIV, LT_EX_SH0_PUB,
+                                                  TR01_PAIRING_KEY_SLOT_INDEX_0);
+    if (LT_OK != ret) {
+        fprintf(stderr, "\nFailed to start Secure Session with key %d, ret=%s\n",
+                (int)TR01_PAIRING_KEY_SLOT_INDEX_0, lt_ret_verbose(ret));
+        fprintf(stderr,
+                "Check if you use correct SH0 keys! Hint: if you use an engineering sample chip, "
+                "compile with "
+                "-DLT_SH0_KEYS=eng_sample\n");
+        return ret;
+    }
+    printf("OK\n");
+
+    // Read I-Config and check if Maintenance Mode is enabled.
+    uint32_t i_config_cfg_startup;
+    printf("  - Reading I-Config[CFG_START_UP]...");
+    ret = lt_i_config_read(lt_handle, TR01_CFG_START_UP_ADDR, &i_config_cfg_startup);
+    if (ret != LT_OK) {
+        fprintf(stderr, "\nFailed to read I-Config[CFG_START_UP], ret=%s\n", lt_ret_verbose(ret));
+        return ret;
+    }
+    printf("OK\n");
+
+    printf("  - Checking if Maintenance Mode is enabled in I-Config[CFG_START_UP]...");
+    if (!(i_config_cfg_startup & BOOTLOADER_CO_CFG_START_UP_MAINTENANCE_ENA_MASK)) {
+        fprintf(stderr,
+                "\nMaintenance Mode is not enabled in I-Config -> FW Update cannot be performed.\n");
+        return LT_FAIL;
+    }
+    printf("OK\n");
+
+    // Read R-Config and check if Maintenance Mode is enabled.
+    // The whole R-Config is read in case we need to modify it, as it has to be completely erased
+    // before writing again.
+    lt_config_t r_config;
+    printf("  - Reading R-Config...");
+    ret = lt_read_whole_R_config(lt_handle, &r_config);
+    if (ret != LT_OK) {
+        fprintf(stderr, "\nFailed to read R-Config, ret=%s\n", lt_ret_verbose(ret));
+        return ret;
+    }
+    printf("OK\n");
+
+    printf("  - Read R-Config (acts as a backup):\n");
+    for (int i = 0; i < LT_CONFIG_OBJ_CNT; i++) {
+        printf("    - %s= 0x%08" PRIx32 "\n", cfg_desc_table[i].desc, r_config.obj[i]);
+    }
+
+    printf("  - Checking if Maintenance Mode is enabled in R-Config[CFG_START_UP]...");
+    if (!(r_config.obj[TR01_CFG_START_UP_IDX] & BOOTLOADER_CO_CFG_START_UP_MAINTENANCE_ENA_MASK)) {
+        printf("Disabled, will enable it\n");
+        printf("    - Erasing R-Config...");
+        ret = lt_r_config_erase(lt_handle);
+        if (ret != LT_OK) {
+            fprintf(stderr, "\nFailed to erase R-Config, ret=%s\n", lt_ret_verbose(ret));
+            return ret;
+        }
+        printf("OK\n");
+
+        r_config.obj[TR01_CFG_START_UP_IDX] |= BOOTLOADER_CO_CFG_START_UP_MAINTENANCE_ENA_MASK;
+        printf("    - Writing modified R-Config...");
+        ret = lt_write_whole_R_config(lt_handle, &r_config);
+        if (ret != LT_OK) {
+            fprintf(stderr, "\nFailed to write R-Config, ret=%s\n", lt_ret_verbose(ret));
+            fprintf(stderr,
+                    "WARNING: R-Config is left in an erased state - the state before erasing is "
+                    "printed above.\n");
+            return ret;
+        }
+        printf("OK\n");
+        g_mtnc_mode_was_enabled = true;
+
+        printf("    - Rebooting TROPIC01 to apply R-Config changes...");
+        ret = lt_reboot(lt_handle, TR01_REBOOT);
+        if (ret != LT_OK) {
+            fprintf(stderr, "\nlt_reboot() failed, ret=%s\n", lt_ret_verbose(ret));
+            fprintf(stderr,
+                    "WARNING: R-Config (with Maintenance Mode enabled) was written but not applied "
+                    "yet. It will be applied on next TROPIC01 reboot/power-cycle.\n");
+            return ret;
+        }
+        printf("OK\n");
+    }
+    else {
+        printf("OK\n");
+    }
+
+    return LT_OK;
+}
+
+/**
+ * @brief  Disables Maintenance Mode.
+ * @param  lt_handle_t Device handle
+ * @retval LT_OK on success
+ */
+lt_ret_t disable_maintenance_mode(lt_handle_t *lt_handle)
+{
+    lt_ret_t ret;
+
+    printf("\nDisabling Maintenance Mode in R-Config (reduces the attack surface):\n");
+
+    printf("  - Starting Secure Session with key slot %d...", (int)TR01_PAIRING_KEY_SLOT_INDEX_0);
+    // Keys are chosen based on the CMake option LT_SH0_KEYS.
+    ret = lt_verify_chip_and_start_secure_session(lt_handle, LT_EX_SH0_PRIV, LT_EX_SH0_PUB,
+                                                  TR01_PAIRING_KEY_SLOT_INDEX_0);
+    if (LT_OK != ret) {
+        fprintf(stderr, "\nFailed to start Secure Session with key %d, ret=%s\n",
+                (int)TR01_PAIRING_KEY_SLOT_INDEX_0, lt_ret_verbose(ret));
+        return ret;
+    }
+    printf("OK\n");
+
+    lt_config_t r_config;
+    printf("  - Reading R-Config...");
+    ret = lt_read_whole_R_config(lt_handle, &r_config);
+    if (ret != LT_OK) {
+        fprintf(stderr, "\nFailed to read R-Config, ret=%s\n", lt_ret_verbose(ret));
+        return ret;
+    }
+    printf("OK\n");
+
+    printf("  - Erasing R-Config...");
+    ret = lt_r_config_erase(lt_handle);
+    if (ret != LT_OK) {
+        fprintf(stderr, "\nFailed to erase R-Config, ret=%s\n", lt_ret_verbose(ret));
+        return ret;
+    }
+    printf("OK\n");
+
+    printf("  - Disabling Maintenance Mode in R-Config...");
+    r_config.obj[TR01_CFG_START_UP_IDX] &= ~BOOTLOADER_CO_CFG_START_UP_MAINTENANCE_ENA_MASK;
+    ret = lt_write_whole_R_config(lt_handle, &r_config);
+    if (ret != LT_OK) {
+        fprintf(stderr, "\nFailed to write R-Config, ret=%s\n", lt_ret_verbose(ret));
+        fprintf(stderr,
+                "WARNING: R-Config is left in an erased state - the state before erasing is printed "
+                "above.\n");
+        return ret;
+    }
+    printf("OK\n");
+
+    printf("  - Rebooting TROPIC01 to apply R-Config changes...");
+    ret = lt_reboot(lt_handle, TR01_REBOOT);
+    if (ret != LT_OK) {
+        fprintf(stderr, "\nlt_reboot() failed, ret=%s\n", lt_ret_verbose(ret));
+        fprintf(stderr,
+                "WARNING: R-Config (with Maintenance Mode disabled) was written but not applied yet. "
+                "It will be applied on next TROPIC01 reboot/power-cycle.\n");
+        return ret;
+    }
+    printf("OK\n");
+
+    printf("  - Verifying that Maintenance Mode is not accessible...");
+    ret = lt_reboot(lt_handle, TR01_MAINTENANCE_REBOOT);
+    if (ret == LT_L2_RESP_DISABLED) {
+        printf("OK\n");
+    }
+    else if (ret != LT_OK) {
+        fprintf(stderr, "\nlt_reboot() failed, ret=%s\n", lt_ret_verbose(ret));
+        return ret;
+    }
+    else {
+        fprintf(stderr, "\nMaintenance reboot succeeded! ret=%s\n", lt_ret_verbose(ret));
+        return ret;
+    }
+
+    return LT_OK;
 }
 
 /**
@@ -336,6 +522,15 @@ int main(void)
     printf("  - RISC-V FW version: %d.%d.%d\n", fw_CPU_ver[3], fw_CPU_ver[2], fw_CPU_ver[1]);
     printf("  - SPECT FW version: %d.%d.%d\n", fw_SPECT_ver[3], fw_SPECT_ver[2], fw_SPECT_ver[1]);
 
+#if LT_DISABLE_MAINTENANCE_MODE
+    // We need to make sure we can reboot into Maintenance Mode to perform the FW update.
+    if (LT_OK != check_and_enable_maintenance_mode(&lt_handle)) {
+        lt_deinit(&lt_handle);
+        mbedtls_psa_crypto_free();
+        return -1;
+    }
+#endif
+
     printf("\nUpdating TROPIC01 firmware...");
     // This helper function implements the recommended FW update process.
     ret = lt_do_mutable_fw_update(&lt_handle, fw_CPU, sizeof(fw_CPU), fw_SPECT, sizeof(fw_SPECT));
@@ -343,6 +538,12 @@ int main(void)
         fprintf(stderr, "\nlt_do_mutable_fw_update() failed, ret=%s\n", lt_ret_verbose(ret));
         fprintf(stderr,
                 "Tip: turn logging on to see more information (compile with -DLT_LOG_LVL=Info)\n");
+#if LT_DISABLE_MAINTENANCE_MODE
+        if (g_mtnc_mode_was_enabled) {
+            fprintf(stderr,
+                    "WARNING: Due to the error, Maintenance Mode was kept enabled in R-Config!\n");
+        }
+#endif
         lt_deinit(&lt_handle);
         mbedtls_psa_crypto_free();
         return -1;
@@ -355,7 +556,23 @@ int main(void)
         return -1;
     }
 
-    printf("Deinitializing handle...");
+#if LT_DISABLE_MAINTENANCE_MODE
+    // Warning: User shall disable Maintenance Mode only after both FW banks are updated with the
+    // latest FW. If only one bank is updated (while the second one contains invalid FW), and
+    // Maintenance Mode is disabled, the probability of bricking TROPIC01 increases. In this case, if
+    // lt_do_mutable_fw_update() succeeds, it is safe to disable Maintenance Mode.
+    if (LT_OK != disable_maintenance_mode(&lt_handle)) {
+        lt_deinit(&lt_handle);
+        mbedtls_psa_crypto_free();
+        return -1;
+    }
+#else
+    printf(
+        "\nWARNING: We strongly recommend disabling Maintenance Mode in R-Config to reduce the attack "
+        "surface.\n");
+#endif
+
+    printf("\nDeinitializing handle...");
     ret = lt_deinit(&lt_handle);
     if (LT_OK != ret) {
         fprintf(stderr, "\nFailed to deinitialize handle, ret=%s\n", lt_ret_verbose(ret));
