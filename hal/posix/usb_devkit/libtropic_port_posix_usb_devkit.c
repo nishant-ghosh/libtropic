d99c591d9368e0f49530345af8806b170f46ba07
andreondra <golasowski.o@gmail.com>
2026-05-11 16:39:00 +0200

    refactor(libtropic_port_posix_usb_devkit): use snprintf

--- hal/posix/usb_devkit/libtropic_port_posix_usb_devkit.c
+++ hal/posix/usb_devkit/libtropic_port_posix_usb_devkit.c
@@ -609,7 +609,9 @@ lt_ret_t lt_port_spi_transfer(lt_l2_state_t *s2, uint8_t offset, uint16_t tx_dat
         // buffered_chars.
         uint8_t buffered_chars[LT_USB_DEVKIT_SPI_TRANSFER_BUFF_SIZE_MAX] = {0};
         for (int i = 0; i < tx_data_length; i++) {
-            int written = sprintf((char *)(buffered_chars + i * 2), "%02" PRIX8, s2->buff[i + offset]);
+            int remaining = sizeof(buffered_chars) - i * 2;
+            int written = snprintf((char *)(buffered_chars + i * 2), remaining, "%02" PRIX8,
+                                   s2->buff[i + offset]);
             if (written < 0) {
                 LT_LOG_ERROR("sprintf failed: errno=%d (%s)", errno, strerror(errno));
                 return LT_HAL_ERROR;
